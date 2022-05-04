ee1cd622c3ad140e94ce8fc83fc1ea695d2a04dd
chenxp <1441173216@qq.com>
2022-05-04 22:52:32 +0800

    后端开发3-1：新增轮播图表，mybatis代码生成相应代码

---
+++ mapper/src/main/java/com/chenxianping/blog/dao/BlogCarouselMapper.java
@@ -0,0 +1,7 @@
+package com.chenxianping.blog.dao;
+
+import com.chenxianping.blog.entity.BlogCarousel;
+import com.chenxianping.blog.general.GeneralDAO;
+
+public interface BlogCarouselMapper extends GeneralDAO<BlogCarousel> {
+}
